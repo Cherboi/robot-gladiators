85efc02924180e58247ee4dced50ab22032e9afc
Cherboi <rplatzer98@gmail.com>
2022-01-24 19:09:04 -0500

    random attack and health values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,14 +22,16 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10)
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -45,8 +47,10 @@ var fight = function(enemyName) {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
-    // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth- damage);
+    
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
@@ -60,6 +64,7 @@ var fight = function(enemyName) {
     }
   }
 };
+
 // function to start a new game
 var startGame = function() {
   // reset player stats
@@ -78,7 +83,7 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
@@ -101,7 +106,6 @@ var startGame = function() {
       break;
     }
   }
-
   // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
   endGame();
 };
@@ -172,6 +176,13 @@ var shop =function() {
       break;
     }
 };
+
+var randomNumber = function() {
+  var value = Math.floor(math.random() * (21)) + 40;
+
+  return value;
+};
+
 
 //start the game when the page loads
 startGame();
